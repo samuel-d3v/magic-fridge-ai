4c127b363d8d228100125940e555f3082907e27d
samuel-d3v <samuel_o.assis@hotmail.com>
2026-02-03 14:17:29 -0300

    feat(db): ajusta o tamanho da coluna categoria em food_item

--- src/main/resources/db/migration/V1__create_initial_schema.sql
+++ src/main/resources/db/migration/V1__create_initial_schema.sql
@@ -3,7 +3,7 @@
 CREATE TABLE food_item (
     id BIGSERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
-    categoria VARCHAR(100) NOT NULL,
+    categoria VARCHAR(30) NOT NULL,
     quantidade INT NOT NULL DEFAULT 0 CHECK (quantidade >= 0),
     created_at TIMESTAMP DEFAULT now(),
     validade DATE NOT NULL CHECK (validade >= CURRENT_DATE),
