6d94ecfaa212edbff0849cceb97fbcbd0b0c106d
samuel-d3v <samuel_o.assis@hotmail.com>
2026-02-10 16:02:02 -0300

    feat(application): habilita @ConfigurationPropertiesScan e @EnableConfigurationProperties

--- src/main/java/br/com/samuel/MagicFridgeAI/MagicFridgeAiApplication.java
+++ src/main/java/br/com/samuel/MagicFridgeAI/MagicFridgeAiApplication.java
@@ -1,9 +1,14 @@
 package br.com.samuel.MagicFridgeAI;
 
+import br.com.samuel.MagicFridgeAI.config.OpenAiProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
+@EnableConfigurationProperties(OpenAiProperties.class)
 public class MagicFridgeAiApplication {
 
 	public static void main(String[] args) {
